9bbcc3455aa4cc848b3c10a4056fea775d9b6a81
Arnab gupta <arnab.gupta@weavers-web.com>
2023-12-28 17:41:05 +0530

    test setup

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
+} from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo.tsx";
+import { useTheme } from "next-themes";
+import { Switch } from "@nextui-org/react";
+import { MoonIcon } from "./MoonIcon.tsx";
+import { SunIcon } from "./SunIcon.tsx";
+import React from "react";
+
+export default function App() {
+  const { theme, setTheme } = useTheme();
+  console.log(theme);
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <AcmeLogo />
+        <p className="font-bold text-inherit">ACME</p>
+      </NavbarBrand>
+
+      <NavbarContent className="sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Switch
+            defaultSelected
+            size="lg"
+            color="secondary"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              if (event.target.checked) {
+                setTheme("dark");
+              } else {
+                setTheme("light");
+              }
+            }}
+            thumbIcon={({ isSelected, className }) =>
+              isSelected ? (
+                <MoonIcon className={className} />
+              ) : (
+                <SunIcon className={className} />
+              )
+            }
+          >
+            Dark mode
+          </Switch>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent as="div" justify="end">
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+    </Navbar>
+  );
+}
